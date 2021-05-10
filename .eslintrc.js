9e5036ceed46ece4259aa47a1e86ee6b2214142e
jopemachine <jopemachine@naver.com>
2021-05-11 02:14:36 +0900

    Update electron-react-boilerplate

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,29 @@
 module.exports = {
-  extends: 'erb/typescript',
+  extends: 'erb',
   rules: {
     // A temporary hack related to IDE not resolving correct package.json
     'import/no-extraneous-dependencies': 'off',
     'import/prefer-default-export': 'off',
     'no-console': 'off',
-    '@typescript-eslint/camelcase': 'off'
+    '@typescript-eslint/camelcase': 'off',
+  },
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+    createDefaultProgram: true,
   },
   settings: {
     'import/resolver': {
       // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
       node: {},
       webpack: {
-        config: require.resolve('./configs/webpack.config.eslint.js')
-      }
-    }
-  }
+        config: require.resolve('./.erb/configs/webpack.config.eslint.js'),
+      },
+    },
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+  },
 };
---
+++ src/main.dev.ts
@@ -0,0 +1,93 @@
+/* eslint global-require: off, no-console: off */
+
+/**
+ * This module executes inside of electron's main process. You can start
+ * electron renderer process from here and communicate with the other processes
+ * through IPC.
+ *
+ * When running `yarn build` or `yarn build-main`, this file is compiled to
+ * `./app/main.prod.js` using webpack. This gives us some performance wins.
+ */
+import path from 'path';
+import { app, BrowserWindow } from 'electron';
+import ElectronStore from 'electron-store';
+import { Core } from 'arvis-core';
+import TrayBuilder from './src/components/tray';
+import { createPreferenceWindow } from './src/windows/preferenceWindow';
+import { createSearchWindow } from './src/windows/searchWindow';
+import { initIPCHandler } from './src/ipc/mainProcessEventHandler';
+import { startFileWatcher } from './src/helper/workflowConfigFileWatcher';
+import installExtensions from './src/config/extensionInstaller';
+
+let preferenceWindow: BrowserWindow | null = null;
+let searchWindow: BrowserWindow | null = null;
+
+const trayIconPath = path.join(__dirname, 'resources', 'icons', '24x24.png');
+const trayBuilder = new TrayBuilder(trayIconPath);
+trayBuilder.buildTray();
+
+ElectronStore.initRenderer();
+
+// If run a specific script with Electron, app name would be Electron. (Not Arvis)
+// To do:: In production, The storage location of the setup file should be Arvis, not Electron.
+// In development env, setting file should be under /Users/me/Library/Application Support/Electron
+// because app.getPath('userData') return 'Electron' as App name.
+app.name = 'Arvis';
+
+if (process.env.NODE_ENV === 'production') {
+  const sourceMapSupport = require('source-map-support');
+  sourceMapSupport.install();
+}
+
+if (
+  process.env.NODE_ENV === 'development' ||
+  process.env.DEBUG_PROD === 'true'
+) {
+  require('electron-debug')();
+} else {
+  // The below code could also be used for testing in production if needed
+  // require('electron-debug')();
+}
+
+app.disableHardwareAcceleration();
+
+app.on('before-quit', () => {
+  if (searchWindow && searchWindow.closable) {
+    searchWindow.close();
+    searchWindow = null;
+    app.exit();
+  }
+});
+
+app.on('ready', async () => {
+  searchWindow = createSearchWindow();
+  preferenceWindow = createPreferenceWindow({ trayBuilder, searchWindow });
+
+  // Open debugging tool by 'undocked'
+  if (
+    process.env.NODE_ENV === 'development' ||
+    process.env.DEBUG_PROD === 'true'
+  ) {
+    installExtensions();
+    searchWindow.webContents.openDevTools({ mode: 'undocked' });
+  }
+
+  startFileWatcher({ searchWindow, preferenceWindow });
+  initIPCHandler({ searchWindow, preferenceWindow });
+
+  // setTimeout(() => {
+  //   //* Transparent window hack (Not working)
+  //   onReadyHandler();
+  // }, 300);
+});
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (preferenceWindow === null) {
+    preferenceWindow = createPreferenceWindow({
+      trayBuilder,
+      searchWindow: searchWindow!,
+    });
+  }
+});
