75c09d06d69fee3c5ea9e572d226a1acdfdeb62c
jopemachine <jopemachine@naver.com>
2021-06-19 02:51:07 +0900

    Update store validator logic

--- src/app/helper/reduxStoreValidator.ts
+++ src/app/helper/reduxStoreValidator.ts
@@ -1,14 +1,21 @@
 /* eslint-disable no-restricted-syntax */
 import { StateType } from '../redux/reducers/types';
+import initialState from '../config/initialState';
 
 export const validate = (store: any): store is StateType => {
   // Assume:: store tree has outer objects and have inner objects in it.
-  const outerKeys = Object.keys(store);
+  const outerKeys = Object.keys(initialState);
   for (const outerKey of outerKeys) {
-    const innerObj = store[outerKey];
+    if (!(store as any)[outerKey]) {
+      console.log(
+        `${outerKey} does not exist in store. Need to reset config..`
+      );
+      return false;
+    }
+    const innerObj = (initialState as any)[outerKey];
     const innerKeys = Object.keys(innerObj);
     for (const innerKey of innerKeys) {
-      if (innerObj[innerKey] === undefined) {
+      if ((store as any)[outerKey][innerKey] === undefined) {
         console.log(
           `${innerKey} does not exist in store. Need to reset config..`
         );
