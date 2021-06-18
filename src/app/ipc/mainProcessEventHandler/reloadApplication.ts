d95bd18f2f3868c0cc9658d89d7c7d191e2776a1
jopemachine <jopemachine@naver.com>
2021-06-19 03:57:17 +0900

    Fix sigabrt error on reload app

--- src/app/ipc/mainProcessEventHandler/reloadApplication.ts
+++ src/app/ipc/mainProcessEventHandler/reloadApplication.ts
@@ -1,12 +1,9 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-/* eslint-disable @typescript-eslint/no-unused-expressions */
 import { IpcMainEvent, app } from 'electron';
 
 /**
  * @param  {string} modifier
  */
 export const reloadApplication = (e: IpcMainEvent) => {
-  // Cause error (SIGABRT) on mac
   app.relaunch();
-  app.exit(0);
+  app.quit();
 };
