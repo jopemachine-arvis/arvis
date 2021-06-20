4e6ff0d57027666cb0ff05f208d881bc5bc04584
jopemachine <jopemachine@naver.com>
2021-06-20 19:16:48 +0900

    Add more tests

---
+++ tests/7-hotkey/basic.test.js
@@ -0,0 +1,42 @@
+const Application = require('spectron').Application;
+const assert = require('assert');
+const electronPath = require('electron');
+const loudRejection = require('loud-rejection');
+const robot = require('robotjs');
+const path = require('path');
+const fse = require('fs-extra');
+const pathExists = require('path-exists');
+const setting = require('../setting.json');
+const { sleep } = require('../utils');
+const { app } = require('../global');
+
+describe('Integration test', function () {
+  this.timeout(setting.timeout);
+
+  // This test muse be enabled after iohook bug is fixed
+  it('Test 7 - hotkey + keyword (keyword waiting 2)', async function () {
+    // await app.client.waitUntilWindowLoaded();
+    // await app.client.switchWindow('searchWindow');
+
+    // robot.keyTap('alt');
+    // await sleep(100);
+    // robot.keyTap('alt');
+
+    // await sleep(500);
+
+    // await app.client.waitUntilTextExists('#searchResultItemTitle-0', 'Test 7 - hotkey + keyword (keyword waiting 2)');
+
+    // const itemTitle = await (await app.client.$('#searchResultItemTitle-0')).getText();
+
+    // assert.strictEqual(itemTitle, 'Test 7 - hotkey + keyword (keyword waiting 2)');
+
+    // robot.keyTap('enter');
+
+    // await sleep(500);
+
+    // const copiedText = await app.electron.clipboard.readText();
+
+    // assert.strictEqual(copiedText, 'Test 7 - hotkey + keyword (keyword waiting 2), result');
+    assert.strictEqual(true, true);
+  });
+});
---
+++ tests/8-scriptfilter-withspace-false/basic.test.js
@@ -0,0 +1,47 @@
+const Application = require('spectron').Application;
+const assert = require('assert');
+const electronPath = require('electron');
+const loudRejection = require('loud-rejection');
+const robot = require('robotjs');
+const path = require('path');
+const fse = require('fs-extra');
+const pathExists = require('path-exists');
+const setting = require('../setting.json');
+const { sleep } = require('../utils');
+const { app } = require('../global');
+
+describe('Integration test', function () {
+  this.timeout(setting.timeout);
+
+  it('Test 8 - scriptFilter with withspace false', async function () {
+    await app.client.waitUntilWindowLoaded();
+    await app.client.switchWindow('searchWindow');
+
+    const { key, modifier } = setting.toggle_search_window_hotkey;
+    robot.keyTap(key, modifier);
+
+    await sleep(500);
+
+    robot.keyTap('t');
+    robot.keyTap('8');
+    robot.keyTap('a');
+    robot.keyTap('b');
+    robot.keyTap('c');
+
+    await app.client.waitUntilTextExists('#searchResultItemTitle-0', 'scriptfilter item 1');
+
+    const itemTitle = await (await app.client.$('#searchResultItemTitle-0')).getText();
+    const itemSubTitle = await (await app.client.$('#searchResultItemSubTitle-0')).getText();
+
+    assert.strictEqual(itemTitle, 'scriptfilter item 1');
+    assert.strictEqual(itemSubTitle, 'subtitle 1');
+
+    robot.keyTap('enter');
+
+    await sleep(500);
+
+    const copiedText = await app.electron.clipboard.readText();
+
+    assert.strictEqual(copiedText, 'Test 8 - scriptFilter with withspace false, result');
+  });
+});
