8c69d836223e4766fe509eff0b7d5da4ffc21b70
jopemachine <jopemachine@naver.com>
2021-06-22 21:58:06 +0900

    Add more tests

---
+++ tests/workflow-integration/t6-scriptfilter-keyword/basic.test.js
@@ -0,0 +1,49 @@
+const Application = require('spectron').Application;
+const assert = require('assert');
+const electronPath = require('electron');
+const loudRejection = require('loud-rejection');
+const robot = require('robotjs');
+const path = require('path');
+const fse = require('fs-extra');
+const pathExists = require('path-exists');
+const setting = require('../../setting.json');
+const { sleep } = require('../../utils');
+const { app } = require('../../global');
+
+describe('Integration test', function () {
+  this.timeout(setting.timeout);
+
+  // To do:: Need to improve async action chain test
+  it('Test 6 - scriptfilter + keyword', async function () {
+    await app.client.waitUntilWindowLoaded();
+    await app.client.switchWindow('searchWindow');
+
+    const { key, modifier } = setting.toggle_search_window_hotkey;
+    robot.keyTap(key, modifier);
+
+    await sleep(500);
+
+    robot.keyTap('t');
+    robot.keyTap('5');
+
+    await app.client.waitUntilTextExists('#searchResultItemTitle-0', 'scriptfilter item 1');
+
+    const itemTitle = await (await app.client.$('#searchResultItemTitle-0')).getText();
+
+    assert.strictEqual(itemTitle, 'scriptfilter item 1');
+
+    robot.keyTap('enter');
+
+    await sleep(500);
+
+    const itemTitle2 = await (await app.client.$('#searchResultItemTitle-0')).getText();
+
+    assert.strictEqual(itemTitle2, 'Test 6 - scriptfilter + keyword, result 1');
+
+    robot.keyTap('enter');
+
+    await sleep(500);
+
+    assert.strictEqual(await app.electron.clipboard.readText(), 'Test 6 - scriptfilter + keyword, result 2');
+  });
+});
