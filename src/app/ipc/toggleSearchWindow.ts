372f7f182f69783fe6d405275041dc2d8b07b7a0
jopemachine <jopemachine@naver.com>
2021-07-06 23:44:59 +0900

    Update trick

--- src/app/ipc/toggleSearchWindow.ts
+++ src/app/ipc/toggleSearchWindow.ts
@@ -1,10 +1,12 @@
 import { screen } from 'electron';
 import { WindowManager } from '../windows';
+import { IPCMainEnum } from './ipcEventEnum';
 
-export default ({ showsUp }: { showsUp?: boolean }) => {
+const handleMacOs = ({ showsUp }: { showsUp?: boolean }) => {
   const searchWindow = WindowManager.getInstance().getSearchWindow();
 
   if (!showsUp && searchWindow.isVisible()) {
+    searchWindow.webContents.send(IPCMainEnum.setSearchbarInput, { str: '' });
     searchWindow.hide();
   } else {
     // Center the window and set y position.
@@ -22,5 +24,70 @@ export default ({ showsUp }: { showsUp?: boolean }) => {
       searchWindow.setOpacity(opacity);
       searchWindow.focus();
     }, 150);
+  }
+};
+
+const handleWindows = ({ showsUp }: { showsUp?: boolean }) => {
+  const searchWindow = WindowManager.getInstance().getSearchWindow();
+
+  if (!showsUp && searchWindow.isVisible()) {
+    searchWindow.webContents.send(IPCMainEnum.setSearchbarInput, { str: '' });
+    searchWindow.hide();
+  } else {
+    // Center the window and set y position.
+    searchWindow.center();
+    const [x] = searchWindow.getPosition();
+    const { height } = screen.getPrimaryDisplay().size;
+
+    // To remove afterimage, move window to unseen position and show
+    searchWindow.setPosition(99999, 99999);
+    searchWindow.show();
+
+    setTimeout(() => {
+      searchWindow.setPosition(x, Math.round(height / 8));
+      searchWindow.focus();
+    }, 150);
+  }
+};
+
+const handleLinux = ({ showsUp }: { showsUp?: boolean }) => {
+  const searchWindow = WindowManager.getInstance().getSearchWindow();
+
+  if (!showsUp && searchWindow.isVisible()) {
+    searchWindow.webContents.send(IPCMainEnum.setSearchbarInput, { str: '' });
+    searchWindow.hide();
+  } else {
+    // Center the window and set y position.
+    searchWindow.center();
+    const [x] = searchWindow.getPosition();
+    const { height } = screen.getPrimaryDisplay().size;
+
+    const [width] = searchWindow.getSize();
+    searchWindow.setBounds({ width: 1 }, false);
+
+    // To remove afterimage, move window to unseen position and show
+    searchWindow.setPosition(99999, 99999);
+    searchWindow.show();
+
+    setTimeout(() => {
+      searchWindow.setBounds({ width }, false);
+      searchWindow.setPosition(x, Math.round(height / 8));
+      searchWindow.focus();
+    }, 150);
+  }
+};
+
+export default ({ showsUp }: { showsUp?: boolean }) => {
+  // eslint-disable-next-line default-case
+  switch (process.platform) {
+    case 'win32':
+      handleWindows({ showsUp });
+      break;
+    case 'darwin':
+      handleMacOs({ showsUp });
+      break;
+    case 'linux':
+      handleLinux({ showsUp });
+      break;
   }
 };
