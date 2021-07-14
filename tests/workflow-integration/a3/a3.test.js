3c023f94541beae126c7fae7bd953137604c9431
jopemachine <jopemachine@naver.com>
2021-07-14 17:53:07 +0900

    hotfix

--- tests/workflow-integration/a3/a3.test.js
+++ tests/workflow-integration/a3/a3.test.js
@@ -1,5 +1,7 @@
 const fse = require('fs-extra');
 const pathExists = require('path-exists');
+const setting = require('../../setting.json');
 const {
+
 
     assert.strictEqual(await getSearchResultCount(app), 3);
