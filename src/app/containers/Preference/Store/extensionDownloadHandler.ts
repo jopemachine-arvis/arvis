2cfb9ae0600021b083ff3aab714d3c0add74fbf0
jopemachine <jopemachine@naver.com>
2021-07-14 23:18:58 +0900

    Fix windows arvis-store bug

--- src/app/containers/Preference/Store/extensionDownloadHandler.ts
+++ src/app/containers/Preference/Store/extensionDownloadHandler.ts
@@ -2,7 +2,20 @@ import { ipcRenderer } from 'electron';
 import execa from 'execa';
 import { downloadExtension } from 'arvis-store';
 import { Core } from 'arvis-core';
+import isAdmin from 'is-admin';
 import { IPCRendererEnum } from '@ipc/ipcEventEnum';
+
+const checkPermissionOnWindows = async () => {
+  if (process.platform === 'win32') {
+    if ((await isAdmin()) === false) {
+      ipcRenderer.send(IPCRendererEnum.showErrorDialog, {
+        title: 'To install extension, run Arvis by administrator permission',
+      });
+      return false;
+    }
+  }
+  return true;
+};
 
 export const installExtension = async ({
   extensionType,
@@ -13,6 +26,8 @@ export const installExtension = async ({
   bundleId: string;
   installType: string;
 }): Promise<void> => {
+  if (!(await checkPermissionOnWindows())) return;
+
   const [creator, name] = bundleId.split('.');
   const env =
     process.platform === 'darwin'
@@ -56,6 +71,8 @@ export const uninstallExtension = async ({
   bundleId: string;
   installType: string;
 }): Promise<void> => {
+  if (!(await checkPermissionOnWindows())) return;
+
   const [creator, name] = bundleId.split('.');
   const env =
     process.platform === 'darwin'
