eebd972ee340fbf995384f3c3b13b9b746878921
jopemachine <jopemachine@naver.com>
2021-08-01 16:20:32 +0900

    Revert window's toggle searchwindow hack

--- src/app/ipc/toggleSearchWindow.ts
+++ src/app/ipc/toggleSearchWindow.ts
@@ -28,6 +28,31 @@ const handleMacOs = ({ showsUp }: { showsUp?: boolean }) => {
 };
 
 const handleWindows = ({ showsUp }: { showsUp?: boolean }) => {
+  // * below code cause intermittent freezing
+
+  // const searchWindow = WindowManager.getInstance().getSearchWindow();
+
+  // if (!showsUp && searchWindow.isVisible()) {
+  //   searchWindow.webContents.send(IPCMainEnum.setSearchbarInput, { str: '' });
+  //   searchWindow.hide();
+  // } else {
+  //   // Center the window and set y position.
+  //   searchWindow.center();
+  //   const [x] = searchWindow.getPosition();
+  //   const { height } = screen.getPrimaryDisplay().size;
+
+  //   // To remove afterimage, move window to unseen position and show
+  //   searchWindow.setPosition(99999, 99999);
+  //   searchWindow.show();
+
+  //   setTimeout(() => {
+  //     searchWindow.setPosition(x, Math.round(height / 8));
+  //     searchWindow.focus();
+  //   }, 150);
+  // }
+
+  // below code includes afterimage
+
   const searchWindow = WindowManager.getInstance().getSearchWindow();
 
   if (!showsUp && searchWindow.isVisible()) {
@@ -39,14 +64,9 @@ const handleWindows = ({ showsUp }: { showsUp?: boolean }) => {
     const [x] = searchWindow.getPosition();
     const { height } = screen.getPrimaryDisplay().size;
 
-    // To remove afterimage, move window to unseen position and show
-    searchWindow.setPosition(99999, 99999);
+    searchWindow.setPosition(x, Math.round(height / 8));
     searchWindow.show();
-
-    setTimeout(() => {
-      searchWindow.setPosition(x, Math.round(height / 8));
-      searchWindow.focus();
-    }, 150);
+    searchWindow.focus();
   }
 };
 
