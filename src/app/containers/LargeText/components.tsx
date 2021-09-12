a9ae14fc9396507059e2d485e48a6a1a23ad8d6a
jopemachine <jopemachine@naver.com>
2021-09-12 23:32:02 +0900

    Fix large text window style bug

--- src/app/containers/LargeText/components.tsx
+++ src/app/containers/LargeText/components.tsx
@@ -6,25 +6,25 @@ export const OuterContainer = styled.div`
   border-radius: 15px;
   color: #fff;
   cursor: move;
+  position: relative;
   display: flex;
+  justify-content: center;
   flex-direction: column;
   flex: 1;
   height: 100vh;
-  justify-content: center;
   overflow-x: hidden;
   overflow-y: scroll;
-  padding-left: 20px;
-  padding-right: 20px;
-  text-align: center;
+  padding-left: 10px;
+  padding-right: 10px;
   width: 100%;
   word-break: break-word;
   -webkit-app-region: drag;
 `;
 
 export const InnerContainer = styled.div`
+  margin-top: auto;
+  margin-bottom: auto;
   cursor: text;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  overflow-y: scroll;
   -webkit-app-region: none;
 `;
